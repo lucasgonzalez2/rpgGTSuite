46695085ad80eb91b5482e5a0fd81e5e0b640cb7
Lucas Vinicius Gonzalez <lucasviniciusgonzalez2002@hotmail.com>
2023-05-24 00:13:53 -0300

    Creating a Creature Class

--- src/base/Creature.java
+++ src/base/Creature.java
@@ -1,3 +1,11 @@
 public class Creature {
-    
+    private String newCreature;
+
+    public Creature(String newCreature) {
+        this.newCreature = newCreature;
+    }
+
+    public String getCreature() {
+       return this.newCreature;
+    }
 }

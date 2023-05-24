201660a85e3eea2e6858aa65d889519a2a6a4c2e
Matheus Tomieiro <38260671+tomieiro@users.noreply.github.com>
2023-05-23 22:23:35 -0300

    feat: add Dice class
    - roll a random value between 1 and the dice sides
    - default the dice to 20 sides

---
+++ src/base/Dice.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Dice {
+    private Random rand;
+    public int sides;
+    private int lastNumber;
+
+    public Dice(int sides) {
+        this.sides = sides;
+        this.rand = new Random();
+    }
+
+    public Dice() {
+        this(20);
+    }
+
+    public int roll(){
+        this.lastNumber = this.rand.nextInt(this.sides) + 1;
+        return this.lastNumber;
+    }
+}

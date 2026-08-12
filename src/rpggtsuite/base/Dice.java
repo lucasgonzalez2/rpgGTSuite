b50a090578766c712b1faa86f6d8cb40dc20faa8
Matheus Tomieiro <38260671+tomieiro@users.noreply.github.com>
2026-08-11 21:48:49 -0300

    refactor: extract Die abstraction and make Dice configurable
    - introduce a Die interface implemented by Dice
    - allow injecting a custom Random for reproducible rolls
    - validate that a die has at least one side

--- src/rpggtsuite/base/Dice.java
+++ src/rpggtsuite/base/Dice.java
@@ -2,22 +2,39 @@ package rpggtsuite.base;
 
 import java.util.Random;
 
-public class Dice {
-    private Random rand;
-    public int sides;
+public class Dice implements Die {
+    public static final int DEFAULT_SIDES = 20;
+
+    private final Random rand;
+    private final int sides;
     private int lastNumber;
 
-    public Dice(int sides) {
-        this.sides = sides;
-        this.rand = new Random();
+    public Dice() {
+        this(DEFAULT_SIDES);
     }
 
-    public Dice() {
-        this(20);
+    public Dice(int sides) {
+        this(sides, new Random());
+    }
+
+    public Dice(int sides, Random rand) {
+        if (sides < 1) {
+            throw new IllegalArgumentException("A die must have at least one side");
+        }
+        this.sides = sides;
+        this.rand = rand;
     }
 
     public int roll() {
         this.lastNumber = this.rand.nextInt(this.sides) + 1;
+        return this.lastNumber;
+    }
+
+    public int getSides() {
+        return this.sides;
+    }
+
+    public int getLastNumber() {
         return this.lastNumber;
     }
 }
---
+++ src/rpggtsuite/base/Die.java
@@ -0,0 +1,7 @@
+package rpggtsuite.base;
+
+public interface Die {
+    int roll();
+
+    int getSides();
+}
